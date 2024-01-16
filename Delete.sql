ade47f4bf0a7c38ccb419aaaaad83e06b8a6e3d0
ehrenato <ehrenato@gmail.com>
2024-01-16 11:21:11 -0300

    update

---
+++ Delete.sql
@@ -0,0 +1,5 @@
+select * from clientes
+
+delete from Clientes
+where Codigo in(1,3)
+
---
+++ Update.sql
@@ -0,0 +1,6 @@
+select * from Clientes where TipoPessoa = 'J' /*só um select*/
+
+update clientes
+set Codigo = 3,
+	Nome = 'Ghi'
+where TipoPessoa = 'J'
